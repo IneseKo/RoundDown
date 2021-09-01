73b567c655efd1e160428d27bfdd56a24dd9e2b4
IneseKo <in.kocina@gmail.com>
2021-09-01 21:06:13 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Scanner input = new Scanner(System.in);
+	System.out.println("please enter number with comma");
+	double number=input.nextDouble();
+	double digit= number;
+	System.out.println("Number:" + digit);
+
+    }
+}
