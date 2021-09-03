972308a2caf0f4a6a5142eb1ebca97ee1183a12f
IneseKo <in.kocina@gmail.com>
2021-09-03 20:14:48 +0300

    Converting grams to kilograms

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,11 +5,18 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	Scanner input = new Scanner(System.in);
-	System.out.println("please enter number with comma");
-	double number=input.nextDouble();
-	double digit= number;
-	System.out.println("Number:" + digit);
+       // Scanner input = new Scanner(System.in);
+        //System.out.println("please enter number with comma");
+        //double number = input.nextDouble();
+        //double digit = number;
+        //System.out.println("Number:" + digit);
+
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("please enter a value in grams");
+        double grams = input.nextDouble();
+        System.out.println(grams/1000 + "kg");
 
     }
+
 }
